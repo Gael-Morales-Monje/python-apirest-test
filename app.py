4cb32f992a966a53ba385fe0848ac91188aea2ae
Gael-Morales-Monje <gaelmoralesmonje@gmail.com>
2024-12-13 21:32:56 -0600

    primer comentario

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+import json
+
+app = Flask(__name__)
+
+
+obj= None
+
+with open("objs.json","r") as file:
+    objs = json.load(file)
+
+
+
+@app.route("/list",methods=["GET"])
+def list():
+    print(objs)
+    return objs,200
+
+
+@app.route("/list",methods=["POST"])
+def Post():
+    list = request.json
+
+    objs.append(list)
+    print(objs)
+    with open("objs.json","w") as file:
+        json.dump(objs, file, indent=4)
+
+    return objs,201
+
+if __name__ == '__main__':
+    app.run(debug=True)
